615434c84d707e22b9993c10d1bdc06b24090062
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-09-21 08:27:58 +0700

    Update MusicRanking.jsx

--- src/components/MusicRanking/MusicRanking.jsx
+++ src/components/MusicRanking/MusicRanking.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import { useStore, actions } from 'store'
 import { createTop20Url } from 'share/utilities'
 import { useCallback } from 'react'
+import { randomId } from 'share'
 
 const MusicRanking = ({ ranking }) => {
   const [state, dispatch] = useStore()
@@ -23,7 +24,7 @@ const MusicRanking = ({ ranking }) => {
       <div className='mr-title main-title-scss'>
         <Link to={createTop20Url('nhac-viet')}>{defineLang('BXH bài hát', 'NCT Song Chart')}</Link>
       </div>
-      <div className='mr-main sm:flex-row ip5:flex-col'>{ranking.map((rankItem) => rankItem && <MusicCard {...rankItem} keyId={rankItem.key} {...musicCardProps} />)}</div>
+      <div className='mr-main sm:flex-row ip5:flex-col'>{ranking.map((rankItem) => rankItem && <MusicCard {...rankItem} key={rankItem.key || randomId(6)} {...musicCardProps} />)}</div>
     </div>
   )
 }
